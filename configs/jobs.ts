ded62bb0fe9a78292d769ad5d359e4093ef15bd5
Rida F'kih <hello@rida.dev>
2022-04-02 16:22:44 -0600

    update jobs

--- configs/jobs.ts
+++ configs/jobs.ts
@@ -4,30 +4,30 @@ const jobs = [
     jobDescription:
       "Reverse-engineering client & network fingerprinting to build out bank connectors.",
     jobStart: new Date(2021, 11),
-    jobTitle: "Financial API Developer",
-  },
-  {
-    companyName: "Cion Studio",
-    jobDescription:
-      "Develop pixel-perfect user interfaces, and architecting backend systems.",
-    jobStart: new Date(2021, 5),
-    jobEnd: new Date(2022, 1),
-    jobTitle: "Fullstack Software Developer",
+    jobTitle: "💰 Financial API Developer",
   },
   {
     companyName: "Jumbleberry",
     jobDescription:
-      "Building the user-interfaces, and some fullstack features of the advertising platform.",
+      "Building the user-interfaces, & fullstack features of their advertising platform.",
     jobStart: new Date(2021, 6),
     jobEnd: new Date(2021, 11),
-    jobTitle: "Intermediate Frontend Developer",
+    jobTitle: "🌐 Intermediate Frontend Developer",
+  },
+  {
+    companyName: "Cion Studio",
+    jobDescription:
+      "Developing pixel-perfect user interfaces, and architecting backend systems.",
+    jobStart: new Date(2021, 5),
+    jobEnd: new Date(2022, 1),
+    jobTitle: "👨‍💻 Fullstack Software Developer",
   },
   {
     companyName: "Code Ninjas",
-    jobDescription: "Taught & inspired the next generation of coders.",
+    jobDescription: "Teaching & inspiring the next generation of coders.",
     jobStart: new Date(2020, 7),
     jobEnd: new Date(2021, 6),
-    jobTitle: "Lead Coding Instructor",
+    jobTitle: "🧑‍🏫 Lead Coding Instructor",
   },
 ];
 
