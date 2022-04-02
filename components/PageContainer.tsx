ec3e842de5e44e700993920d3ca10964bea3241a
Rida F'kih <hello@rida.dev>
2022-04-02 16:22:34 -0600

    updating spacing on page container

--- components/PageContainer.tsx
+++ components/PageContainer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PageContainer: React.FC = ({ children }) => (
-  <main className="px-24 py-12 space-y-8">{children}</main>
+  <main className="px-24 py-12 space-y-12">{children}</main>
 );
 
 export default PageContainer;
