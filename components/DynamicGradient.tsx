cfad97c6dfdf81a4ca91c49e5c480b8e31bdf485
Rida F'kih <hello@rida.dev>
2022-04-02 23:22:08 -0600

    add fancy dynamic gradient

---
+++ components/DynamicGradient.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface CircleProps {
+  color: string;
+}
+
+const Circle: React.VFC<CircleProps> = ({ color }) => {
+  return (
+    <div
+      className="w-1/2 rounded-full h-1/2 first:scale-125 last:scale-125"
+      style={{ backgroundColor: color }}
+    />
+  );
+};
+
+const DynamicGradient: React.VFC = () => {
+  const colors = ["#2DDAFC", "#FCB72D", "#F52DFC", "#FC2D68"];
+
+  return (
+    <div className="absolute inset-0 flex flex-wrap m-auto pointer-events-none -z-10 blur-[10rem] opacity-60 scale-75">
+      {colors.map((color) => {
+        return <Circle color={color} key={color} />;
+      })}
+    </div>
+  );
+};
+
+export default DynamicGradient;
--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import Link from "next/link";
 
 import Section from "@/components/Section";
+import DynamicGradient from "@/components/DynamicGradient";
 
 const Footer: React.VFC = () => {
   return (
     <Section>
       <footer className="relative h-[75vh] flex flex-col items-center justify-center text-center">
+        <DynamicGradient />
         <h2 className="py-8 text-5xl">Have a challenge for me?</h2>
         <p className="max-w-[48ch]">
           I occasionally take on new opportunities.
