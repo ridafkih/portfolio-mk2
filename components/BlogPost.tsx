d86fd97bccd7669e03666a4fa07383603b6a1722
Rida F'kih <hello@rida.dev>
2022-04-03 00:49:47 -0600

    add timestamp to blog posts

--- components/BlogPost.tsx
+++ components/BlogPost.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 
 import Heading from "@/components/Heading";
 
@@ -9,14 +9,27 @@ interface BlogPostProps {
 }
 
 const BlogPost: React.VFC<BlogPostProps> = ({ title, description, date }) => {
+  const { current: loadedDate } = useRef<Date>(new Date());
+
+  const getDaysBetweenDates = () => {
+    const timeDifference = Math.abs(loadedDate.getTime() - date.getTime());
+    return Math.ceil(timeDifference / (1000 * 60 * 60 * 24));
+  };
+
   return (
-    <div className="space-y-8">
+    <div className="relative transition-transform hover:-translate-y-1">
       <div className="flex items-center p-6 -mx-6 border rounded-md border-neutral-300 dark:border-neutral-700">
         <div className="space-y-2">
           <Heading type="h3">{title}</Heading>
           <p className="text-sm leading-7 opacity-80">{description}</p>
         </div>
       </div>
+      <time
+        className="absolute bottom-0 right-0 px-4 py-2 text-xs transform translate-y-[1rem] bg-neutral-900"
+        dateTime={`P${getDaysBetweenDates()}D`}
+      >
+        {getDaysBetweenDates()} days ago.
+      </time>
     </div>
   );
 };
