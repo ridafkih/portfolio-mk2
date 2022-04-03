1caf43eedc656fb692060130dd9b07c8b591744f
Rida F'kih <hello@rida.dev>
2022-04-03 00:53:01 -0600

    -- semantics

--- configs/blogs.ts
+++ configs/blogs.ts
@@ -1,6 +1,6 @@
 const blogs = [
   {
-    title: "How not to expose the personal data of 19,577 Canadians.",
+    title: "How to not expose the personal data of 19,577 Canadians.",
     description:
       "A month ago, I spoke to CBC news about PORTpass. An attempt by a local entrepreneur to bring digital proof-of-vaccine to Canadians. I have spoken to many about PORTpass, and their failure...",
     date: new Date(),
