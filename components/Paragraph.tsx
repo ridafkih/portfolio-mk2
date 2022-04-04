342868b844b4f1c35e54f21282eff4e51d87bbf2
Rida F'kih <hello@rida.dev>
2022-04-04 00:24:40 -0400

    update paragraph component props

--- components/Paragraph.tsx
+++ components/Paragraph.tsx
@@ -1,7 +1,28 @@
 import React from "react";
 
-const Heading: React.FC = ({ children }) => (
-  <p className="font-normal leading-7">{children}</p>
+const paragraphWeights = {
+  normal: "font-normal",
+  medium: "font-medium",
+  semibold: "font-semibold",
+  bold: "font-bold",
+};
+
+interface ParagraphProps {
+  characterLimit?: number;
+  weight?: keyof typeof paragraphWeights;
+}
+
+const Paragraph: React.FC<ParagraphProps> = ({
+  children,
+  characterLimit,
+  weight = "normal",
+}) => (
+  <p
+    className={`leading-7 ${paragraphWeights[weight]}`}
+    style={characterLimit ? { maxWidth: `${characterLimit}ch` } : {}}
+  >
+    {children}
+  </p>
 );
 
-export default Heading;
+export default Paragraph;
