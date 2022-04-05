082fda3ee511759a4220485926a9db399a377ec0
Rida F'kih <hello@rida.dev>
2022-04-05 01:30:25 -0400

    add placeholder screens

---
+++ pages/blog.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const BlogPage: NextPage = () => {
+  return <div></div>;
+};
+
+export default BlogPage;
---
+++ pages/photos.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from "next";
+
+const PhotosPage: NextPage = () => {
+  return <div></div>;
+};
+
+export default PhotosPage;
