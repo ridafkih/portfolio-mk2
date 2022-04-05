5467d3b0665a2e3e81ea67ed51d0edef04906c01
Rida F'kih <hello@rida.dev>
2022-04-05 01:30:38 -0400

    add navigation & PLINK

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,27 @@
 import React from "react";
 
 import CurrentlyPlaying from "@/components/CurrentlyPlaying";
+import Navigation from "./Navigation";
+
+const navItems = [
+  {
+    name: "Home",
+    path: "/",
+  },
+  {
+    name: "Blog",
+    path: "/blog",
+  },
+  {
+    name: "Photos",
+    path: "/photos",
+  },
+];
 
 const Header: React.VFC = () => {
   return (
-    <header className="flex justify-end px-8 py-12 sm:px-24">
+    <header className="flex justify-between px-8 py-12 sm:px-24">
+      <Navigation items={navItems} />
       <CurrentlyPlaying />
     </header>
   );
---
+++ components/Navigation.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import usePlink from "@/hooks/usePlink";
+
+interface NavItem {
+  name: string;
+  path: string;
+}
+
+interface NavigationProps {
+  items: NavItem[];
+}
+
+const Navigation: React.VFC<NavigationProps> = ({ items }) => {
+  const router = useRouter();
+  const plink = usePlink();
+
+  const handleClick = plink.play;
+
+  return (
+    <nav>
+      <ul className="flex gap-4">
+        {items.map(({ name, path }) => {
+          const isOnPage = router.asPath === path;
+          const weightClass = isOnPage
+            ? "font-normal opacity-100"
+            : "font-thin opacity-80";
+
+          return (
+            <li key={name}>
+              <Link href={path} passHref>
+                <a
+                  onClick={!isOnPage ? handleClick : void 0}
+                  className={`cursor-pointer after:bg-transparent transition-opacity hover:opacity-100 ${weightClass}`}
+                >
+                  {name}
+                </a>
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
