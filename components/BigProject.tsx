01387a968b4dba792cf586379e5cf086e456c8da
Rida F'kih <hello@rida.dev>
2022-04-04 23:59:42 -0400

    implement link, adjust for dark mode

--- components/BigProject.tsx
+++ components/BigProject.tsx
@@ -20,17 +20,17 @@ const BigProject: React.VFC<BigProjectProps> = ({
   containerClassName = "",
 }) => {
   return (
-    <Link href="" passHref>
+    <Link href={link} passHref>
       <a
         target="big_project"
-        className={`group block p-6 space-y-4 sm:-mx-6 rounded-md bg-neutral-900 text-white gap-4 after:bg-transparent ${containerClassName}`}
+        className={`group block p-6 space-y-4 sm:-mx-6 rounded-md dark:bg-neutral-100 bg-neutral-900 text-white gap-4 after:bg-transparent ${containerClassName}`}
       >
         <Heading type="h3">
-          <span className="text-transparent bg-gradient-to-r from-pink-400 to-orange-400 bg-clip-text">
+          <span className="text-transparent bg-gradient-to-r from-pink-500 to-orange-500 bg-clip-text">
             {name}
           </span>
         </Heading>
-        <div className="flex flex-col justify-between gap-4 md:flex-row">
+        <div className="flex flex-col justify-between gap-4 md:flex-row dark:text-neutral-900">
           <Paragraph>{description}</Paragraph>
           <div className="flex items-center justify-end gap-2 -translate-x-1">
             <span className="font-light">Learn more</span>
