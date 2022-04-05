35b40df6ad10700b1a1ebd29f890b4e560c8be30
Rida F'kih <hello@rida.dev>
2022-04-05 01:35:26 -0400

    add placeholders for other pages

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,20 @@
 import { NextPage } from "next";
+import Head from "next/head";
+
+import WidthLimiter from "@/components/WidthLimiter";
+import Header from "@/components/Header";
 
 const BlogPage: NextPage = () => {
-  return <div></div>;
+  return (
+    <>
+      <Head>
+        <title>Rida F&apos;kih — Blog</title>
+      </Head>
+      <WidthLimiter>
+        <Header />
+      </WidthLimiter>
+    </>
+  );
 };
 
 export default BlogPage;
