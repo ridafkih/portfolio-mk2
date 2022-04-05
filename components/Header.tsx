810b6069fb095560ab55d0792ca3daf5a947d904
Rida F'kih <hello@rida.dev>
2022-04-05 01:58:50 -0400

    fix import

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import CurrentlyPlaying from "@/components/CurrentlyPlaying";
-import Navigation from "./Navigation";
+import Navigation from "@/components/Navigation";
 
 const navItems = [
   {
