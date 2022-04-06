74e1a6e96f211f157e0db424a5b6af77961a7d0b
Rida F'kih <hello@rida.dev>
2022-04-06 18:09:30 -0400

    adjust scrawl styling

--- components/Scrawl.tsx
+++ components/Scrawl.tsx
@@ -16,11 +16,16 @@ const Scrawl: React.FC<ScrawlProps> = ({ children, date }) => {
     });
 
   return (
-    <li className="flex flex-col py-2 border-b rounded-md sm:px-4 sm:-mx-4">
+    <li className="flex flex-col-reverse gap-4 py-2 border-b rounded-md sm:px-4 sm:-mx-4">
       <Paragraph>
         <span className="text-xl font-cursive">&quot;{children}&quot;</span>
       </Paragraph>
-      <time dateTime={date.toString()}>{getParsedDate()}</time>
+      <time
+        className="text-xs text-neutral-500 sm:text-right"
+        dateTime={date.toString()}
+      >
+        {getParsedDate()}
+      </time>
     </li>
   );
 };
