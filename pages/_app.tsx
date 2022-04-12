54af0c7046b6b0228905cc053f00541aa02b84f0
Rida F'kih <hello@rida.dev>
2022-04-12 00:16:41 -0600

    feat: :heavy_plus_sign: add posthog analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import "../styles/globals.css";
 import { useRouter } from "next/router";
 import type { AppProps } from "next/app";
 
+import { usePostHog } from "next-use-posthog";
+
 import MetaData from "@/components/MetaData";
 
 import { getCurrentUrl } from "@/utils/url";
@@ -10,6 +12,10 @@ import { SpotifyProvider } from "@/contexts/SpotifyContext";
 
 const MyApp: React.VFC<AppProps> = ({ Component, pageProps }) => {
   const router = useRouter();
+
+  usePostHog("phc_Eha0Zvd4PTi3HOq5nqS9GisOI13f2pt0zB7J6iPP8oE", {
+    api_host: "https://app.posthog.com",
+  });
 
   return (
     <>
