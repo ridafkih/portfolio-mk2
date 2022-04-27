af9343902f308d9c53707b8b73676d7288aa3472
Rida F'kih <hello@rida.dev>
2022-04-27 13:56:29 -0600

    chore: :speech_balloon: fix typo

--- sections/IntroSection.tsx
+++ sections/IntroSection.tsx
@@ -10,7 +10,7 @@ const IntroSection: React.VFC = () => (
     <Heading type="h1">Hello, I&apos;m Rida F&apos;kih 👋</Heading>
     <Paragraph weight="light">
       I&apos;m a Canadian software developer based in Calgary, Alta., I learned
-      to code make my life a little easier. I currently work{" "}
+      to code in order to make my life a little easier. I currently work{" "}
       <Link href="https://techcrunch.com/2021/09/10/maxrewards-banks-3m-to-reveal-best-payment-methods-that-reap-the-most-rewards/">
         @ MaxRewards
       </Link>{" "}
