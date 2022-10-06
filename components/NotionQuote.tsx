0b856daf30bfe962aea7cef2777c1e7d893bb725
Rida F'kih <hello@rida.dev>
2022-10-05 23:48:17 -0600

    fix quote min width

--- components/NotionQuote.tsx
+++ components/NotionQuote.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const NotionQuote: NotionBlockComponent<NotionBlock.QUOTE> = (props) => {
   return (
     <blockquote className="flex items-center italic font-light whitespace-pre-line">
-      <div className="self-stretch w-1 py-8 mr-4 bg-neutral-900 dark:bg-neutral-50" />
+      <div className="self-stretch min-w-[0.15rem] py-8 mr-4 bg-neutral-900 dark:bg-neutral-50" />
       {props.quote.rich_text[0].plain_text}
     </blockquote>
   );
