860e4c4e4105bdeb04d2d7e34676082c93eb6f3b
Rida F'kih <hello@rida.dev>
2022-10-06 00:32:23 -0600

    fix new badge to actually work

--- sections/BlogListSection.tsx
+++ sections/BlogListSection.tsx
@@ -22,17 +22,21 @@ const BlogListSection: React.VFC<BlogListSectionProps> = ({ blogs }) => {
   return (
     <ul className="flex flex-col gap-2">
       {blogs.map(({ title, description, created, url }) => {
+        const createdDate = stringToDate(created);
+        const oldDate = new Date().getTime() - (1000 * 60 * 60 * 24 * 30);
+        const isNew = oldDate <= createdDate.getTime();
+        
         return (
           <li
-            key={stringToDate(created).getTime().toString()}
+            key={createdDate.getTime().toString()}
             className="relative transition"
           >
             <Link href={url} passHref>
               <a className="cursor-pointer">
                 <div className="px-4 py-4 space-y-2 transition-[background-color] rounded-md sm:-mx-4 dark:bg-neutral-900 dark:hover:bg-neutral-800 hover:bg-neutral-100 bg-neutral-50">
-                  <div className="p-0.5 px-2 bg-amber-200 text-amber-900 text-xs w-fit rounded-full">
+                  {isNew && <div className="p-0.5 px-2 bg-amber-200 text-amber-900 text-xs w-fit rounded-full">
                     New
-                  </div>
+                  </div>}
                   <time className="block text-xs font-semibold text-neutral-400">
                     {parseDate(stringToDate(created))}
                   </time>
