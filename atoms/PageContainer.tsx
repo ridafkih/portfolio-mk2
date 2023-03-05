037fd9dcd02a9b7e5328cd95373e6d03a0a0a0d6
Rida <hello@rida.dev>
2023-03-05 15:36:29 -0700

    reduce spacing between sections

--- atoms/PageContainer.tsx
+++ atoms/PageContainer.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const PageContainer: React.FC = ({ children }) => (
-  <main className="px-8 pb-24 space-y-12 sm:space-y-24 sm:pt-12 sm:px-24">
+  <main className="px-8 pb-24 space-y-12 sm:space-y-12 sm:pt-12 sm:px-24">
     {children}
   </main>
 );
