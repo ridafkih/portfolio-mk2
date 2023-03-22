332b61cc1780987a3bf2205a23731616892dc215
Rida <hello@rida.dev>
2023-03-22 20:19:50 +0100

    remove double period

--- sections/AboutMyselfSection.tsx
+++ sections/AboutMyselfSection.tsx
@@ -7,7 +7,7 @@ import Section from "@/atoms/Section";
 const AboutMyselfSection = () => (
   <Section title="About Myself 🤘">
     <Paragraph weight="light">
-      <strong className="font-semibold">I like to make things.</strong>. When
+      <strong className="font-semibold">I like to make things</strong>. When
       presented with a challenge I do and learn what it takes to build the
       solution. I like quick iterations and prototype-driven work. I am
       motivated by interesting projects, and I would rather get things done than
