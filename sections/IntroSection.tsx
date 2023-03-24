522c6078e4f5639b293a7d51b6d275214bf5652a
Rida F'kih <hello@rida.dev>
2023-03-24 14:58:18 +0100

    Update IntroSection.tsx

--- sections/IntroSection.tsx
+++ sections/IntroSection.tsx
@@ -9,12 +9,10 @@ const IntroSection: React.VFC = () => (
   <Section>
     <Heading type="h1">Hello, I&apos;m Rida F&apos;kih 👋</Heading>
     <Paragraph weight="light">
-      I&apos;m a Canadian software developer based in Calgary, Alta., I learned
-      to code in order to make my life a little easier. I currently work{" "}
+      Young nomadic full-stack mobile, web, & reverse engineer currently{" "}
       <Link href="https://techcrunch.com/2021/09/10/maxrewards-banks-3m-to-reveal-best-payment-methods-that-reap-the-most-rewards/">
         @ MaxRewards
-      </Link>{" "}
-      as a reverse-engineer &amp; fullstack developer.
+      </Link>.
     </Paragraph>
   </Section>
 );
