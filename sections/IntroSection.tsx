53395e130036217d3cc7132476ccfcb28537d6a0
Rida F'kih <hello@rida.dev>
2023-09-20 14:37:22 -0400

    Update IntroSection.tsx

--- sections/IntroSection.tsx
+++ sections/IntroSection.tsx
@@ -10,8 +10,8 @@ const IntroSection: React.VFC = () => (
     <Heading type="h1">Hello, I&apos;m Rida F&apos;kih 👋</Heading>
     <Paragraph weight="light">
       Young full-stack mobile, web, & reverse engineer currently{" "}
-      <Link href="https://techcrunch.com/2021/09/10/maxrewards-banks-3m-to-reveal-best-payment-methods-that-reap-the-most-rewards/">
-        @ MaxRewards
+      <Link href="https://texts.com/">
+        @ Texts.com
       </Link>.
     </Paragraph>
   </Section>
